39476b3e0217ce290563a3eccd07f92ddf4ab4d1
Nathan Pire <nathan.pire@gmail.com>
2025-07-31 17:02:38 +0200

    working on updating to angular 18

--- projects/ng2-canvas-whiteboard/karma.conf.js
+++ projects/ng2-canvas-whiteboard/karma.conf.js
@@ -20,6 +20,10 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
+    coverageReporter: {
+      type: 'html',
+      dir: 'coverage/',
+    },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
